e4037774758651dd32709aa1824eb0f171c4d7b6
magdaTrzTrash <mwt.trash@gmail.com>
2022-12-27 21:46:24 +0100

    przycisk read more

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,17 +1,27 @@
-import React from "react";
+import {useState} from "react";
 import "../styles/MissionsListStyles.css";
 
 const MissiosnList = (props) => {
+
+    const [isReadMoreShown, setReadMoreShown] = useState(false);
+
+    const toggleBtn = () => {
+        setReadMoreShown(prevState => !prevState);
+    };
+
     return (
         <div className="missions">{
             props.errorDisplay ? (
                 <p>Erorr: {props.errorDisplay} </p>
             ) : (
                 props.missionsDisplay.map(mission => (
-                    <div>
+                    <div className=" mission">
                         <h2>{mission.mission_name}</h2>
-                        <p>{mission.description}</p>
-                    </div>
+                        <p>{mission.description.substr(0, 200)}</p>
+                        {/* {isReadMoreShown ? mission.description : mission.description.substr(0, 200)}*/
+                        
+                        <button onClick={toggleBtn}> {isReadMoreShown ? 'Czytaj mniej...' : '...Czytaj wiecej'}</button>}
+                    </div> 
                 ))
             )
         }
